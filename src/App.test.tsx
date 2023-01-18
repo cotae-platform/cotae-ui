3f00573ebac2822bdbff3c45c4d98d3c534c85c2
PangHae <yds05074@gmail.com>
2023-01-18 18:06:50 +0900

    initial commit v2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@src/setupTests";
-import App from "@src/App";
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import "@src/setupTests";
+// import App from "@src/App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+export {};
